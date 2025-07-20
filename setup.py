d353154bffa55d8fb8dcd0e8a1f0c9853cd2bd88
deepankdixit <deepankdixit0804@gmail.com>
2025-07-20 21:46:13 +0530

    changes to setup.py and req

--- setup.py
+++ setup.py
@@ -9,5 +9,14 @@ setup(
     name="document_portal",
     version="0.1",
     author="Deepank Dixit",
-    packages=find_packages(),
+    packages=find_packages(), # auto-discovers packages
+    install_requires = requirements #won't have to install requirements.txt separately. Only need to run pip install -e .
 )
+
+"""
+1. About find_packages() in setup.py
+
+The find_packages() function (from setuptools) automatically discovers all packages and sub-packages in your project directory.
+It does this by looking for directories that contain an __init__.py file (which marks them as Python packages).
+This saves you from manually listing each package in the packages argument of setup().
+"""
